31c4761b6765057c96c20d09455d96333adf76b1
Taimoor Qureshi <taimooralqureshi@gmail.com>
2020-04-05 23:28:58 +0500

    saviour model type and itemlists add

--- models/saviour-model.js
+++ models/saviour-model.js
@@ -1,23 +1,17 @@
 const mongoose = require("../db");
 var uniqueValidator = require('mongoose-unique-validator');
-const item = {
-    name : String,
-    unit: Number,
-    quantity: Number
-}
 
-const schema = {
+const saviourSchema = mongoose.Schema({
     name : {type: String, required: true, min:3, trim: true},
     phone : {type: mongoose.SchemaTypes.String, required: true, trim: true, unique:true, index:true},
     location: {type: String},
     password : {type: String, required: true, min: 6},
     createdOn: { type: Date, default: Date.now()},
     type : {type: String, default: "individual"},
-    itemlist: [item] 
-};
+    itemlists: [mongoose.SchemaTypes.Mixed]
+});
 
 const collectionName = "saviours";
-const saviourSchema = mongoose.Schema(schema);
 saviourSchema.plugin(uniqueValidator);
 const saviour = mongoose.model(collectionName, saviourSchema);
 
