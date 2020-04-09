270540cad672e02c7fb5e6f0ee85b0c9341148de
Taimoor Qureshi <taimooralqureshi@gmail.com>
2020-04-09 23:58:32 +0500

    validation added

--- models/seeker-model.js
+++ models/seeker-model.js
@@ -1,13 +1,14 @@
 const mongoose = require("../db");
 const uniqueValidator = require('mongoose-unique-validator');
 const schema = {
-    name: { type: String, required: true },
-    phone : { type: String, required: true },
-    nic : { type: String, required: true, max:13, min:13, unique:true, index:true},
-    location: { type: String},
-    createdOn : {type: Date, default: Date.now()},
-    UpdateOn : {type: Date, required:Date },
-    status : {type: Boolean, default:true}
+    name        : { type: String, required: [true, 'name is required!'] },
+    phone       : { type: String, required: [true, 'phone number is required!'] },
+    nic         : { type: String, required: [true, 'nic is required!'], 
+                    match: [/\d{13}/,'must be 13-digit number'], max:13, min:13, unique:true, index:true},
+    location    : { type: String},
+    createdOn   : { type: Date, default: Date.now()},
+    UpdateOn    : { type: Date, required:Date },
+    status      : { type: Boolean, default:true}
 };
 
 const collectionName = "seekers"; // Name of the collection of documents
