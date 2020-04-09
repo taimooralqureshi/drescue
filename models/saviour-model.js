9ddc6d264974aeb87d37db25eb279b2ca0864156
Taimoor Qureshi <taimooralqureshi@gmail.com>
2020-04-09 23:34:07 +0500

    validation added

--- models/saviour-model.js
+++ models/saviour-model.js
@@ -2,13 +2,14 @@ const mongoose = require("../db");
 var uniqueValidator = require('mongoose-unique-validator');
 
 const saviourSchema = mongoose.Schema({
-    name : {type: String, required: true, min:3, trim: true},
-    phone : {type: mongoose.SchemaTypes.String, required: true, trim: true, unique:true, index:true},
-    location: {type: String},
-    password : {type: String, required: true, min: 6},
-    createdOn: { type: Date, default: Date.now()},
-    type : {type: String, default: "individual"},
-    itemlists: [mongoose.SchemaTypes.Mixed]
+    name        : { type: String, required: [true, 'name is required'], min:3, trim: true},
+    phone       : { type: String, required: [true, 'phone number is required'], 
+                    match: [/\d{11}/, 'number must be 11-digit'], trim: true, unique:true, index:true},
+    location    : { type: String},
+    password    : { type: String, required: [true, 'password is required'], min: 6},
+    createdOn   : { type: Date, default: Date.now()},
+    type        : { type: String, default: "individual"},
+    itemlists   : [ mongoose.SchemaTypes.Mixed]
 });
 
 const collectionName = "saviours";
